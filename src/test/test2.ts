36c814ffece63e8cd5565585836555c944bca142
4423-mtg <tricoroiidx@gmail.com>
2026-03-29 03:17:42 +0900

    remove deprecated code

--- src/test/test2.ts
+++
@@ -1,8 +0,0 @@
-import path from "node:path";
-import { fetchOracleCardsBulkData } from "../lib/scryfall.js";
-
-// await downloadOracleCards();
-
-const directory = String.raw`./data`;
-console.log(path.parse(directory));
-0;
